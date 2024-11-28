202759988fd06121160e5e75e4b1752809900576
vishal11u <shitolevishal29@gmail.com>
2024-11-28 11:21:53 +0530

    change database connect code

--- server.js
+++ server.js
@@ -18,8 +18,8 @@ app.use(cors());
 
 mongoose
   .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true,
   })
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error(err));
